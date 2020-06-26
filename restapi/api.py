a2c6580d62fa95afe0766730bde6db9a39bc2f26
Joshua Green <joshua.green@klarna.com>
2020-06-26 21:36:47 +0200

    create REST API

---
+++ restapi/api.py
@@ -0,0 +1,23 @@
+import csv
+import sys
+from bottle import route, run, template
+
+@route('/recommendations/<visitorid>')
+def recommendations(visitorid):
+    if visitorid in model:
+        return {'visitorid': visitorid,
+                'itemids':   model[visitorid]}
+    else:
+        abort(404, 'Invalid visitorid')
+
+def load_model(filename):
+    model = {}
+    with open(filename, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            model[row[0]] = row[1:]
+    return model
+
+if __name__ == '__main__':
+    model = load_model(sys.argv[1])
+    run(host='localhost', port=8080)
