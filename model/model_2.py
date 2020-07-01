5f5c7238f1399e5d277f1b7233c98f62997ba966
Joshua Green <joshua.green@klarna.com>
2020-07-02 00:14:52 +0200

    generate recommendations based on model

--- model/model_2.py
+++ model/model_2.py
@@ -26,13 +26,13 @@ def add_event_scoring(events_df):
     events_df['score'] = events_df.event.replace(category_scores)
     return events_df
 
-def train_test_split(df, ratio=0.8):
+def train_test_split(df, ratio=0.95):
     split_point = int(df.shape[0] * ratio)
     df_train    = df.iloc[0:split_point]
     df_test     = df.iloc[split_point:]
 
-    df_test=df_test[(df_test['visitorid'].isin(df_train['visitorid']))&\
-                     (df_test['itemid'].isin(df_train['itemid']))]
+    df_test = df_test[(df_test['visitorid'].isin(df_train['visitorid'])) &
+                      (df_test['itemid'].isin(df_train['itemid']))]
 
     return df_train, df_test
 
@@ -53,17 +53,31 @@ def make_interaction_matrices(events_df):
 def make_model(interaction_matrix):
     # use WARP since only positive interactions are present
     model = LightFM(loss='warp')
-    model.fit(interaction_matrix, epochs=1, num_threads=2)
+    model.fit(interaction_matrix, epochs=10, num_threads=2)
     return model
 
 def measure_model(model, test_im):
     test_auc  = auc_score(model, test_im, num_threads=4)
     return test_auc.mean()
 
-def recommendations_for_user(model, userid, itemids, count=100):
-    recommendations = model.predict(userid, itemids)
-    recommendations[::-1].sort()
-    return recommendations[:count]
+def get_recommendation_visitorids(filename):
+    df = pd.read_csv(filename)
+    return df.visitorid
+
+def make_recommendations(model, visitorids, itemids, count=100):
+    recommendations = []
+    for i, visitorid in enumerate(visitorids):
+        scores = model.predict(visitorid, itemids)
+        top_items = itemids[np.argsort(-scores)]
+        recommendations.append([visitorid] + list(top_items[:count]))
+        print(f'progress: {i} of {len(visitorids)}', end='\r')
+    return recommendations
+
+def save_recommendations_csv(recommendations, filename):
+    df = pd.DataFrame(recommendations)
+    headers = ['visitorid'] + [f'item_{i}' for i in range(0, len(df.columns) - 1)]
+    df.columns = headers
+    df.to_csv(filename)
 
 if __name__ == '__main__':
     edf         = clean_events('source_data/events.csv')
@@ -71,5 +85,8 @@ if __name__ == '__main__':
     train, test = make_interaction_matrices(edf)
     model       = make_model(train)
     mean_auc    = measure_model(model, test)
+    print('test AUC:', mean_auc)
 
-
+    visitorids  = get_recommendation_visitorids('source_data/predictions.csv')
+    itemids     = edf.itemid.unique()
+    recs        = make_recommendations(model, visitorids[:10], itemids)
