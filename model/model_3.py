cc8ed704786cc4a0e26d29d11a68c7684af67883
Joshua Green <joshua.green@klarna.com>
2020-07-02 00:17:07 +0200

    create 3rd iteration model

--- model/model_3.py
+++ model/model_3.py
@@ -22,6 +22,20 @@ def extract_categories_from_properties(properties_file):
     cats = df.rename(columns={'value': 'category'})
     return cats.sort_values(by='timestamp').reset_index(drop=True)
 
+def make_interaction_matrices(events_df):
+    train, test = train_test_split(events_df)
+
+    n_visitors = events_df.visitorid.size
+    n_items    = events_df.itemid.size
+
+    train_im = coo_matrix((train.score, (train.visitorid, train.itemid)),
+                          shape=(n_visitors, n_items))
+    test_im  = coo_matrix((test.score, (test.visitorid, test.itemid)),
+                          shape=(n_visitors, n_items))
+    train_im.sum_duplicates()
+    test_im.sum_duplicates()
+    return train_im, test_im
+
 def join_events_to_categories(event_df, category_df):
     '''join event and category data, using timestamp to find
        the category that applied at the time of the event'''
@@ -38,7 +52,15 @@ def join_events_to_categories(event_df, category_df):
     df = df.drop(['timestamp_cat', 'tstamp_diff'], axis=1)
     return df.reset_index(drop=True)
 
+def make_model(interaction_matrix, item_features):
+    model = LightFM(loss='warp')
+    model.fit(interaction_matrix, item_features=item_features,
+              epochs=10, num_threads=2)
+    return model
+
 if __name__ == '__main__':
     edf = clean_events('source_data/events.csv')
     cdf = extract_categories_from_properties('source_data/item_properties.csv')
     df  = join_events_to_categories(edf, cdf)
+    train, test = make_interaction_matrices(df)
+    model = make_model(train, df.category)
